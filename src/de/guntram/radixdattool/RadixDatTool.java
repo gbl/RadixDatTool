219c9dea471a397626f25a6dd2491e27411e1c3e
Guntram Blohm <gbl@bso2001.com>
2016-03-26 11:44:18 +0100

    extract bitmaps into subdirectories; ascii-art-display of object bitmaps

--- src/de/guntram/radixdattool/RadixDatTool.java
+++ src/de/guntram/radixdattool/RadixDatTool.java
@@ -9,6 +9,8 @@ import java.io.EOFException;
 import java.io.File;
 import java.io.IOException;
 import java.io.RandomAccessFile;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  *
@@ -16,16 +18,45 @@ import java.io.RandomAccessFile;
  */
 public class RadixDatTool {
 
-    /**
-     * @param args the command line arguments
-     */
+    final static String dataDir="/home/gbl/Oldgames";
+    final static String expandSubdir="Radix_expanded";
+    
+
     public static void main(String[] args) throws IOException {
-        String inputFileName="/home/gbl/Oldgames/RADIX.DAT";
+        
+        String whatToDo;
+        String file;
+        
+        // whatToDo="extract";
+        // file=dataDir+File.separatorChar+"RADIX.DAT";
+        
+        whatToDo="obitmap";
+        file=dataDir+File.separatorChar+expandSubdir+File.separatorChar+
+                "ObjectBitmaps"+File.separatorChar+"Nuke1-32-85";
+
+        if (args.length==2) {
+            whatToDo=args[0];
+            file=args[1];
+        }
+        
+        if (whatToDo.equals("extract")) {
+            extract(file);
+        } else if (whatToDo.equals("obitmap")) {
+            obitmap(file);
+        }
+    }
+    
+    
+    public static void extract(String inputFileName) throws IOException {
         RandomAccessFile inputFile=new RandomAccessFile(inputFileName, "r");
         inputFile.seek(0x11);
         int fileCount=endianSwap(inputFile.readInt());
         int indexPos=endianSwap(inputFile.readInt());
         System.out.println(""+fileCount+" file names at: "+Long.toHexString(indexPos));
+        
+        new File(dataDir+File.separatorChar+expandSubdir).mkdirs();
+        new File(dataDir+File.separatorChar+expandSubdir+File.separatorChar+"ObjectBitmaps").mkdirs();
+        new File(dataDir+File.separatorChar+expandSubdir+File.separatorChar+"WallBitmaps").mkdirs();
         
         byte[] fileEntryBytes=new byte[46];
         for (int n=0;;n++) {
@@ -50,19 +81,29 @@ public class RadixDatTool {
                 for (int j=0; j<bitmapsCount; j++) {
                     inputFile.seek(descriptorsPos+j*40);
                     inputFile.readFully(bitmapDescriptor);
-                    fileName=getStringAt(bitmapDescriptor, 0, 32);
+                    String fileName2=getStringAt(bitmapDescriptor, 0, 32);
                     int bitmapFilePos = getDwordAt(bitmapDescriptor, 32);
-                    int bitmapP1=getWordAt(bitmapDescriptor, 36);
-                    int bitmapP2=getWordAt(bitmapDescriptor, 38);
-                    System.out.print(String.format("  %3d: %-33s: %08x %3d %3d", j, fileName, bitmapFilePos, bitmapP1, bitmapP2));
-                    if (j>0) {
-                        System.out.print(String.format("%8d after previous", bitmapFilePos-lastBitmapFilePos));
+                    int width=getWordAt(bitmapDescriptor, 36);
+                    int height=getWordAt(bitmapDescriptor, 38);
+                    int nBytes;
+                    System.out.println(String.format("  %3d: %-33s: %08x %3d %3d", j, fileName2, bitmapFilePos, width, height));
+                    if (fileName.equals("WallBitmaps")) {
+                        nBytes=width*height;
+                    } else {
+                        nBytes=width*height;
+                        fileName2+="-"+width+"-"+height;
                     }
-                    lastBitmapFilePos=bitmapFilePos;
-                    System.out.println();
+                    String outputSubDir=dataDir+File.separatorChar+expandSubdir+File.separatorChar+fileName;
+                    if (fileName2.indexOf('/')>0) {
+                        String outputSubSubDir=outputSubDir+File.separatorChar+fileName2.substring(0, fileName2.lastIndexOf('/'));
+                        System.out.println("making dir "+outputSubSubDir);
+                        new File(outputSubSubDir).mkdirs();
+                    }
+                    copy(outputSubDir+File.separatorChar+fileName2,
+                            inputFile, bitmapFilePos, nBytes);
                 }
             } else {
-                // copy("/home/gbl/Oldgames/Radix_expanded/"+fileName, inputFile, startPos, fileSize);
+                copy(dataDir+File.separatorChar+expandSubdir+File.separatorChar+fileName, inputFile, startPos, fileSize);
             }
         }
     }
@@ -72,6 +113,40 @@ public class RadixDatTool {
                ((x&0x00ff0000)>>>8)  |
                ((x&0x0000ff00)<<8)   |
                ((x&0x000000ff)<<24);
+    }
+    
+    public static void obitmap(String inputFileName) throws IOException {
+        int width, height;
+        char[] byteMap=new char[256];
+        char nextMap='!';
+        Matcher m=(Pattern.compile(".*-(\\d+)-(\\d+)$").matcher(inputFileName));
+        if (m.find()) {
+            System.out.println(m.group(1));
+            width=Integer.parseInt(m.group(1));
+            System.out.println(m.group(2));
+            height=Integer.parseInt(m.group(2));
+        } else {
+            throw new IOException("Filename does not contain width/height");
+        }
+        RandomAccessFile inputFile=new RandomAccessFile(inputFileName, "r");
+        int bytes=(int) inputFile.length();
+        byte[] content=new byte[bytes];
+        inputFile.readFully(content);
+        for (int i=0; i<height; i++) {
+            int pos=getWordAt(content, i*4);
+            int first=getByteAt(content, i*4+2);
+            int count=getByteAt(content, i*4+3);
+            for (int j=0; j<first; j++) {
+                System.out.print(' ');
+            }
+            for (int j=0; j<count; j++) {
+                int color=getByteAt(content, pos++);
+                if (byteMap[color]==0)
+                    byteMap[color]=nextMap++;
+                System.out.print(byteMap[color]);
+            }
+            System.out.println();
+        }
     }
     
     private static int getByteAt(byte[] data, int pos) {
